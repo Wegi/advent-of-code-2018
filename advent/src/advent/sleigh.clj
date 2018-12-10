f858f72eb8358703ccea64d2bc868392d9fd6445
Alexander Schneider <alexander@schneider.gg>
2018-12-10 09:42:24 +0100

    Add day 7.2

--- advent/src/advent/sleigh.clj
+++ advent/src/advent/sleigh.clj
@@ -53,13 +53,93 @@
       complete-dag
       topological-sort))
 
-(part-1 instructions)
-
-(topological-sort (complete-dag (build-dag instructions)))
+#_(part-1 instructions)
 ;;BCEFLDMQTXHZGKIASVJYORPUWN
+;; ;; ;; ;; ;; Part 2 ;; ;; ;; ;; ;;
+(defn- filter-finished-workers
+  [workers]
+  (filter #(< 0 (:time-left %)) workers))
 
-(build-dag instructions)
+(defn- completed-projects
+  [workers]
+  (map :project
+       (filter #(= 0 (:time-left %)) workers)))
 
+(defn- update-clocks
+  [workers]
+  (map #(update % :time-left dec) workers))
+
+(defn- time-for
+  [node]
+  (+ (int (first node)) -4))
+
+(defn- in-progress
+  [workers]
+  (into #{} (map :project workers)))
+
+
+(defn assign-workers
+  "Assume a maximum of 5 separate workers."
+  [workers queue]
+  (let [limit 5
+        new-workers (remove nil? (map (fn [node] (when-not ((in-progress workers) node)
+                                                  {:project node
+                                                   :time-left (time-for node)}))
+                                      (take limit queue)))]
+    (concat workers (take (- limit (count workers)) new-workers))))
+
+(defn remove-all-satisfied
+  [dag completed]
+  (reduce #(remove-satisfied %2 %1) dag completed))
+
+(defn- stupid-fullify
+  [dag]
+  (let [all-letters (into #{} (flatten (concat (keys dag) (vals dag))))]
+    (reduce (fn [dag key]
+              (if (contains? dag key)
+                dag
+                (assoc dag key [])))
+            dag all-letters)))
+
+(defn teamwork
+  ([dag]
+   (teamwork dag #{}))
+  ([dag not-in]
+   (loop [time -1
+          rest-dag dag
+          completed not-in
+          queue (next-candidates rest-dag)
+          workers []]
+     (if (and (empty? rest-dag) (empty? workers))
+       time
+       (let [temp-workers (update-clocks workers)
+             new-completed (set (concat completed (completed-projects temp-workers)))
+             new-dag (remove-all-satisfied rest-dag new-completed)
+             new-queue (next-candidates new-dag)
+             new-workers (assign-workers (filter-finished-workers temp-workers) new-queue)]
+         (recur (inc time) new-dag new-completed new-queue new-workers))))))
+
+(defn part-2
+  [instructions]
+  (-> instructions
+      build-dag
+      stupid-fullify
+      teamwork))
+
+#_(part-2 instructions)
+;; 987
+
+(def small-instructions
+  "Step C must be finished before step A can begin.
+Step C must be finished before step F can begin.
+Step A must be finished before step B can begin.
+Step A must be finished before step D can begin.
+Step B must be finished before step E can begin.
+Step D must be finished before step E can begin.
+Step F must be finished before step E can begin.")
+
+#_(part-2 small-instructions)
+;;999
 (def instructions
   "Step L must be finished before step M can begin.
 Step B must be finished before step S can begin.
